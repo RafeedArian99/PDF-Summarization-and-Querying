7141e33eb4a4482ecf53627f85547b3084096a89
Shreyas Kirtane <skirtan1@asu.edu>
2024-04-28 17:38:24 -0700

    adding prompt template

--- summarizing.py
+++ summarizing.py
@@ -38,7 +38,8 @@ class Summarizer:
 
     def __init__(self, model, tokenizer):
 
-
+        self.map_template = map_template
+        self.reduce_template = reduce_template
         self.text_splitter = RecursiveCharacterTextSplitter(
             # Set a really small chunk size, just to show.
             chunk_size=4000,
@@ -61,20 +62,16 @@ class Summarizer:
             generation_config=generation_config,
         )
 
-        llm = HuggingFacePipeline(pipeline=text_pipeline, model_kwargs={"temperature": 0})
         self.llm = HuggingFacePipeline(pipeline=text_pipeline, model_kwargs={"temperature": 0})
 
 
 
-
-        map_prompt = PromptTemplate.from_template(map_template)
-        map_chain = LLMChain(llm=llm, prompt=map_prompt)
-        map_prompt = PromptTemplate.from_template(map_template)
+        map_prompt = PromptTemplate.from_template(self.map_template)
         self.map_chain = LLMChain(llm=self.llm, prompt=map_prompt)
 
 
         
-        reduce_prompt = PromptTemplate.from_template(reduce_template)
+        reduce_prompt = PromptTemplate.from_template(self.reduce_template)
 
         self.reduce_chain = LLMChain(llm=self.llm, prompt=reduce_prompt)
 
@@ -148,65 +145,3 @@ class Summarizer:
         """
         res = self.map_chain.run(self.file_to_docs(file))
         return textwrap.fill(res, width=100)
-
-
-
-# class Queryer:
-#     """
-#     Create a new Queryer that will initialize embeddings and LLM
-#     """
-
-#     def __init__(self) -> None:
-#         print(end="Initializing... ", flush=True)
-#         self.embeddings = HuggingFaceEmbeddings()
-#         repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
-#         self.llm = HuggingFaceEndpoint(repo_id=repo_id, temperature=0.7)
-#         self.chain = None
-#         self.chat_history = []
-#         print("DONE")
-
-#     def process_file(self, file_path: str) -> None:
-#         """
-#         Pass the path to a PDF file for the Queryer to process.
-#         It may take some time for large PDF's.
-#         """
-#         raw_text = ""
-#         pdfreader = PdfReader(file_path)
-
-#         for page in tqdm(pdfreader.pages):
-#             content = page.extract_text()
-#             if content:
-#                 raw_text += content
-
-#         text_splitter = RecursiveCharacterTextSplitter(
-#             separators=["\n", "."],
-#             chunk_size=800,
-#             chunk_overlap=200,
-#             length_function=len,
-#         )
-#         texts = text_splitter.split_text(raw_text)
-
-#         print(end="Creating vector store... ", flush=True)
-#         vector_store = FAISS.from_texts(texts, self.embeddings)
-#         print("DONE!")
-
-#         retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 5})
-#         self.chain = ConversationalRetrievalChain.from_llm(
-#             llm=self.llm, chain_type="stuff", retriever=retriever
-#         )
-
-#     def ask(self, query: str) -> str:
-#         """
-#         Pass a question for the Queryer. Returns the LLM's response.
-#         """
-#         if not query or query.isspace():
-#             return None
-
-#         # query += "\n\nPlease refrain from repeating yourself."
-#         start_time = time.time()
-#         print(end="Generating response...", flush=True)
-#         response = self.chain.invoke({"question": query, "chat_history": self.chat_history})
-#         response = response["answer"].strip()
-#         self.chat_history.append((query, response))
-#         print(f"DONE ({time.time() - start_time}s)")
-#         return response
